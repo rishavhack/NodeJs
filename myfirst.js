5f97bfe7c086bf5443e3327dcf3fc93da543ccb6
Rishav <rishav.jnit@gmail.com>
2018-05-25 22:25:17 +0530

    Node

--- myfirst.js
+++ myfirst.js
@@ -3,3 +3,19 @@ http.createServer(function(req,res){
 	res.writeHead(200,{'Content-Type':'text/html'});
 	res.end('Hello World');
 }).listen(8080);
+
+var robot = require("robotjs");
+ 
+// Speed up the mouse.
+robot.setMouseDelay(2);
+ 
+var twoPI = Math.PI * 2.0;
+var screenSize = robot.getScreenSize();
+var height = (screenSize.height / 2) - 10;
+var width = screenSize.width;
+ 
+for (var x = 0; x < width; x++)
+{
+    y = height * Math.sin((twoPI * x) / width) + height;
+    robot.moveMouse(x, y);
+}
